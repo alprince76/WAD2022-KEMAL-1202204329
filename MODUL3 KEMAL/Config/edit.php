12c4e1266377bad53f04800f53963771428da85e
Kemal Aziz <azizkemal599@gmail.com>
2022-11-23 08:14:05 +0700

    debugging ulang lagi

--- MODUL3 KEMAL/Config/edit.php
+++ MODUL3 KEMAL/Config/edit.php
@@ -1,6 +1,6 @@
 <?php
 
-include 'Connector.php';
+include 'insert.php';
 
  if (isset($_POST['edit'])) {
     $id = $_POST['id'];
@@ -15,7 +15,7 @@ include 'Connector.php';
 
     if ($file_tmp) {
         move_uploaded_file($file_tmp, '../Asset/'.$file);
-        $query = "UPDATE showroom_kemal_table SET nama_mobil = '$namaMob', pemilik_mobil = '$date', merk_mobil = '$brand', tanggal_beli = '$tanggal_beli', deskripsi = '$description', foto_mobil = '$file', status_pembayaran = '$status' id_mobil = $id";
+        $query = "UPDATE showroom_kemal_table SET nama_mobil = '$namaMob', pemilik_mobil = '$date', merk_mobil = '$brand', tanggal_beli = '$tanggal_beli', deskripsi = '$description', foto_mobil = '$file', status_pembayaran = '$status' WHERE id_mobil = $id";
         $result = mysqli_query($conn, $query);
     } else {
         $query = "UPDATE showroom_kemal_table SET nama_mobil = '$namaMob', pemilik_mobil = '$date', merk_mobil = '$brand', tanggal_beli = '$tanggal_beli', deskripsi = '$description', status_pembayaran = '$status' WHERE id_mobil = $id";
